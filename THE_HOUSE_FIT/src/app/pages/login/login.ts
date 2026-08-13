fbe68b9da7d4d61cee5b2d42a8280ffe52faca8a
daniel suarez <pilodaniel4000@gmail.com>
2026-08-13 13:37:11 -0500

    se realiza dos modulos mas los services

---
+++ THE_HOUSE_FIT/src/app/pages/login/login.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule, RouterLink, NgIf],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class LoginComponent {
+  email = 'admin@thehousefit.com';
+  password = '123456';
+  mensaje = '';
+
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+  ) {}
+
+  login(): void {
+    const autenticado = this.authService.inciarSesion(this.email, this.password);
+
+    if (!autenticado) {
+      this.mensaje = 'Correo o contraseña incorrectos.';
+      return;
+    }
+
+    this.router.navigate(['/panel/dashboard']);
+  }
+}
---
+++ THE_HOUSE_FIT/src/app/services/auth.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor() {}
+
+  // Agrega aquí tus métodos de autenticación (ej. registro, login, etc.)
+  registro(datosUsuario: any) {
+    // Lógica o petición HTTP
+  }
+}
