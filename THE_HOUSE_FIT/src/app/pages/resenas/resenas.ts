50d9e738ea0b866369dfc766b2f601074adf1216
Daniel-Gomez101 <danielgomez12pinzon@gmail.com>
2026-08-14 14:32:11 -0500

    se creo  del compoente resenas el html

---
+++ THE_HOUSE_FIT/src/app/pages/resenas/resenas.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resenas',
+  imports: [],
+  templateUrl: './resenas.html',
+  styleUrl: './resenas.css',
+})
+export class Resenas {}
