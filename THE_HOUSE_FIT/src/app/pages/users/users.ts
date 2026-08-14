944e13324492cee961b0c2e5fdf48043ddf692bd
cristianlozano5286-beep <cristianlozano5286@gmail.com>
2026-08-14 13:26:16 -0500

    se añaden las importaciones en el user.ts

--- THE_HOUSE_FIT/src/app/pages/users/users.ts
+++ THE_HOUSE_FIT/src/app/pages/users/users.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgForOf, NgIf } from '@angular/common';
+
+
+
+interface Usuario {
+  id: number;
+  nombre: string;
+  apellido: string;
+  correo: string;
+  rol: string;
+  estado: boolean;
+}
+
+
+const STORAGE_KEY='usuariosSistema';
 
 @Component({
   selector: 'app-users',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, NgForOf, NgIf],
   templateUrl: './users.html',
   styleUrl: './users.css',
 })
-export class Users {}
