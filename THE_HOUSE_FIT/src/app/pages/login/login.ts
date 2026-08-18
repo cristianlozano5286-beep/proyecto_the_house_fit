42cb604bb9d77ef96b4935bad476f452224faa18
cristianlozano5286-beep <cristianlozano5286@gmail.com>
2026-08-18 17:21:04 -0500

    se terminan de hacer arreglos en login css

--- THE_HOUSE_FIT/src/app/pages/login/login.ts
+++ THE_HOUSE_FIT/src/app/pages/login/login.ts
@@ -1,46 +1,27 @@
-
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { NgIf } from '@angular/common';
-
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { NgIf } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-
   imports: [FormsModule, RouterLink, NgIf],
-
-  imports: [FormsModule, RouterLink],
-
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class LoginComponent {
-
-  email = 'admin@thehousefit.com';
-  password = '123456';
-  mensaje = '';
-
-  constructor(
-    private router: Router,
-    private authService: AuthService,
-  ) {}
-
-
+  // Inyección de servicios
   private router = inject(Router);
   private authService = inject(AuthService);
 
-  email: string = '';
-  password: string = '';
+  // Variables para el formulario
+  email: string = 'admin@thehousefit.com';
+  password: string = '123456';
   mensaje: string = '';
 
   // Método ejecutado al presionar ingresar (HU10, HU11)
-
   login(): void {
     const autenticado = this.authService.inciarSesion(this.email, this.password);
 
@@ -48,11 +29,6 @@ export class LoginComponent {
       this.mensaje = 'Correo o contraseña incorrectos.';
       return;
     }
-
-
-    this.router.navigate(['/panel/dashboard']);
-  }
-}
 
     // Redirección tras inicio de sesión exitoso
     this.router.navigate(['/dashboard']);
@@ -62,4 +38,3 @@ export class LoginComponent {
     this.router.navigate(['/register']);
   }
 }
-
