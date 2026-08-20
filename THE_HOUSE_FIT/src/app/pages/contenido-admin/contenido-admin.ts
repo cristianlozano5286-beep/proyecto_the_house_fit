2545295fbde16aab0224b786af6ee58b50d6fe61
cristiangaitan17 <cristiangaitan1717@icloud.com>
2026-08-20 14:58:55 -0500

    se crea un nuevo componente y se hace el html

---
+++ THE_HOUSE_FIT/src/app/pages/contenido-admin/contenido-admin.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contenido-admin',
+  imports: [],
+  templateUrl: './contenido-admin.html',
+  styleUrl: './contenido-admin.css',
+})
+export class ContenidoAdmin {
+
+}
