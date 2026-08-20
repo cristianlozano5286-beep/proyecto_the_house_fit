3b03397d453b2778c2c69c753d9867588557b311
cristiangaitan17 <cristiangaitan1717@icloud.com>
2026-08-20 15:04:12 -0500

    se crea al componente de catalogo de gimnasios y se hace la estructura HTML3

---
+++ THE_HOUSE_FIT/src/app/pages/catalogo-gimnasios/catalogo-gimnasios.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalogo-gimnasios',
+  imports: [],
+  templateUrl: './catalogo-gimnasios.html',
+  styleUrl: './catalogo-gimnasios.css',
+})
+export class CatalogoGimnasios {
+
+}
